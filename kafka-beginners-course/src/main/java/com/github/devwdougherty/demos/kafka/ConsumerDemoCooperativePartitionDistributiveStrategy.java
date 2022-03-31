614a0ccdfa12af837d768e1b62f1ddf7f74b3635
Willian Barbosa <willian@qindin.com.br>
2022-03-31 10:05:28 -0300

    kafka beginners course

---
+++ kafka-beginners-course/src/main/java/com/github/devwdougherty/demos/kafka/ConsumerDemoCooperativePartitionDistributiveStrategy.java
@@ -0,0 +1,80 @@
+package com.github.devwdougherty.demos.kafka;
+
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.errors.WakeupException;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class ConsumerDemoCooperativePartitionDistributiveStrategy {
+
+    public static final Logger log = LoggerFactory.getLogger(ConsumerDemoCooperativePartitionDistributiveStrategy.class);
+
+    public static void main(String[] args) {
+
+        System.out.println("I'm a Kafka Consumer!");
+
+        String bootStrapServer = "127.0.0.1:9092";
+        String groupId = "my-third-application";
+        String topic = "demo_java";
+
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootStrapServer);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest"); //none/earliest/latest
+        properties.setProperty(ConsumerConfig.PARTITION_ASSIGNMENT_STRATEGY_CONFIG, CooperativeStickyAssignor.class.getName());
+        // properties.setProperty(ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, ""); For static assignment of consumers to partitions.
+
+        // Create consumer
+        KafkaConsumer<String, String> consumer=  new KafkaConsumer<>(properties);
+
+        // Get a reference to the current thread
+        final Thread mainThread = Thread.currentThread();
+
+        // Adding the shutdown hook
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                log.info("Detected a shutdown, let's exit by calling consumer.wakeup()...");
+                consumer.wakeup();
+
+                // Join the main thread to allow the execution of the code in the main thread
+                try {
+                    mainThread.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        try {
+            // Subscribe consumer to our topic(s)
+            consumer.subscribe(Collections.singletonList(topic));
+
+            // Pool for new data
+            while (true) {
+
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
+
+                for (ConsumerRecord<String, String> record : records) {
+
+                    log.info("Key: " + record.key() + ", Value: " + record.value());
+                    log.info("Partition: " + record.partition() + ", Offset: " + record.offset());
+                }
+            }
+        } catch (WakeupException e) {
+            log.info("Wake up exception!");
+            // We ignore this as this is an expected exception when closing a consumer
+        } catch (Exception e) {
+            log.error("Unexpected exception");
+        } finally {
+            consumer.close(); // this will also commit the offsets if need be
+            log.info("The consumer is now gracefully closed");
+        }
+    }
+}
